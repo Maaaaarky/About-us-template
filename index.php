c63e5ad16cae1f75e8c04e638062faad96ba118c
Maaaaarky <131601385+Maaaaarky@users.noreply.github.com>
2023-05-22 14:41:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The Mind Mechanics - About Us</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+	  .header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }s
+
+        .header .logo {
+            display: flex;
+            align-items: center;
+        }
+
+        .header .logo img {
+            width: 30px; /* Adjust the width as desired */
+            height: 30px; /* Adjust the height as desired */
+            margin-right: 10px; /* Add some spacing between the logo and header title */
+        }
+
+        .header .logo h1 {
+            margin: 0; /* Remove the default margin */
+        }
+
+	  .second-header img {
+            max-width: 100%; /* Set the maximum width of the image */
+            height: 150px; /* Maintain the aspect ratio */
+        }
+
+        .about-us {
+            text-align: center;
+            margin-top: 120px;
+	  	margin-left: 1in;
+    		margin-right: 1in;
+        }
+	
+	  .who-we-are {
+            text-align: center;
+            margin-top: 20px;
+	  	margin-left: 1in;
+    		margin-right: 1in;
+        }
+
+	  .team {
+            text-align: center;
+            margin-top: 20px;
+	  	margin-left: 1in;
+    		margin-right: 1in;
+        }
+        .contact-us {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .contact-info {
+            margin-top: 20px;
+		margin-bottom: 100px;
+        }
+    </style>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="Logo.png" alt="The Mind Mechanics Logo">
+            <h1>The Mind Mechanics</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">FAQ's</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sign out</a></li>
+                <li><a href="#">Doctors</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <header class="second-header">
+        <img src="Banner.png" alt="Header Image">
+    </header>
+    
+    <section class="about-us">
+        <h2>ABOUT US</h2>
+        <p>
+            Welcome to The Mind Mechanics! We are a mental health care provider committed to helping individuals improve their mental well-being and live happier, more fulfilling lives.
+        </p>
+        <p>
+            With our care, we offer a range of services including therapy, counseling, and psychiatric evaluations. Our dedicated team of professionals is experienced in addressing various mental health conditions and providing evidence-based treatment approaches.
+        </p>
+        <p>
+            At The Mind Mechanics, we prioritize the well-being of our clients and aim to create a safe and compassionate environment for their healing journey. Our mission is to empower individuals to overcome their mental health challenges and build resilience.
+        </p>
+    </section>
+
+    <section class="who-we-are">
+        <h2>WHO WE ARE</h2>
+        <p>
+            We are doctors, with different specialties ready to assist and keep our lives mentally, physically, socially, and emotionally healthy. Live without regrets! We are here to provide your mental needs as we safely secure your confidential information away from the cause of your difficulties. Don't worry as we are professionaly trained and all of us are cum laudes to assure you that our services aces the country's needs.
+        </p>
+    </section>
+
+	<section class="team">
+        <h2>TEAM</h2>
+        <p>
+            We strive to create a safe and compassionate environment for our clients, where they feel heard, understood, and supported. Your ability to actively listen, show empathy, and provide evidence-based treatment approaches is instrumental in helping individuals navigate their mental health challenges.
+        </p>
+	  <p>
+	      Remember, each interaction you have with our clients can make a profound difference in their lives. Your words of encouragement, guidance, and therapeutic interventions have the power to bring about positive change and transform their mental well-being.
+	  </p>
+    </section>
+
+    <section class="contact-us">
+        <hr>
+        <h2>Contact Us</h2>
+        <div class="contact-info">
+            <div class="contact-item">
+                <strong>Contact No:</strong> +639265296849
+            </div>
+            <div class="contact-item">
+                <strong>Location:</strong> Santa Clara Street, Manila, Philippines
+            </div>
+            <div class="contact-item">
+                <strong>Email Address:</strong> themindmechanics@gmail.com
+            </div>
+        </div>
+    </section>
+</body>
+</html>
